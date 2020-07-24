5e4b65488403dd7e446696308f76bb2bd4ab32c0
Ritesh Kumar <riteshk.296@gmail.com>
2020-07-25 02:11:20 +0530

    Update code_checkout.sh

--- scripts/code_checkout.sh
+++ scripts/code_checkout.sh
@@ -1,7 +1,8 @@
 if ! ls /opt | grep -qF "code"; then
 	mkdir /opt/code
-    echo "Creating New Code Diretcory"
+    	echo "Creating New Code Diretcory"
 else
+	rm -rf /opt/code/.
 	echo "Code directoty already present"
 fi
 cp -ap $WORKSPACE/* /opt/code/ 
